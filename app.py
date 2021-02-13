84a0c53ef77558ee8745e29f56854e9d9c0b32fd
Lam Teng <lamteng@hotmail.com>
2021-02-13 21:28:47 +0000

    added new class

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-#3:28:47
+# 3:28:47
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
---
+++ app1.py
@@ -0,0 +1,41 @@
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1)
+student2 = Student("Pam", "Art", 2.5)
+
+print(student1.name)
+print(student2.gpa)
+print(student1.major)
+
+from Question import Question
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are Bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+questions = [
+    Question(question_prompts[0],"a"),
+    Question(question_prompts[1],"c"),
+    Question(question_prompts[2],"b")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(questions)
+
+from Student import Student
+student1 = Student("Oscar", "Accounting", 3.1)
+student2 = Student("Phyllis", "Business", 3.8)
+
+print(student2.on_honor_roll())
+
+from Chef import Chef
+
+myChef = Chef()
+myChef.make_special_dish()
